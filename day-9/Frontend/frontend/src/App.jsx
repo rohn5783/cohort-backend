242f4b687de7514ff54aed413244ef7e10b198ea
Rohit Pandey <rp5120523@gmail.com>
2026-02-07 04:58:33 +0530

    day 9 completed

--- day-9/Frontend/frontend/src/App.jsx
+++ day-9/Frontend/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
@@ -28,18 +28,73 @@ function App() {
       description: "this is my sixth note",
     },
   ]);
-axios.get("http://localhost:3000/notes").then((res) => {
+  console.log("hello integration")
+function fetchNotes() {
+  axios.get("http://localhost:3000/notes").then((res) => {
   setnote(res.data.notes);
 });
+}
+useEffect(()=> {
+  fetchNotes();
+},[])
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const {title, description} = e.target.elements;
+  console.log(title.value, description.value)
+  axios.post("http://localhost:3000/notes", {
+    title: title.value,
+description: description.value
+  }).then((res) => {
+    console.log(res.data);
+    fetchNotes();
+  });
+
+
+
+
+
+}
+function handleDelete(noteId) {
+  
+  console.log(noteId)
+  axios.delete(`http://localhost:3000/notes/${noteId}`).then((res) => {
+    console.log(res.data);
+    fetchNotes();
+  });
+
+
+}
+
+function handleUpdate(noteId,oldDescription) {
+  console.log(noteId)
+  const newDescription = prompt("Enter new description", oldDescription);
+  axios.patch(`http://localhost:3000/notes/${noteId}`, {
+    description: newDescription || oldDescription
+  }).then((res) => {
+    console.log(res.data);
+    fetchNotes();
+  });
+}
+
 
   return (
     <>
+    <form className="note-create-form" onSubmit={handleSubmit}>
+      <input name="title" type="text" placeholder=" Enter title" />
+      <input name="description" type="text" placeholder=" Enter description" />
+
+      <button type="submit">Create Note</button>  
+    </form>
       <div className="notes">
         {note.map((note) => {
           return (
             <div className="note">
               <h1>{note.title}</h1>
               <p>{note.description}</p>
+      <button type="button" className="delete" onClick={() => handleDelete(note._id)}>Delete Note</button>
+      <button type="button" className="edit" onClick={() => handleUpdate(note._id, note.description)}>Update Note</button>
+
             </div>
           );
         })}
